12368e28ff208bd5d16533f4ddd37650221f4a1a
teerapongchaemchamrat <juejuechaemchamrat@gmail.com>
2023-07-22 15:33:29 +0700

    main

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import './App.css';
 //import { Detail } from './components/detail';
 //import { Layout } from './components/layout';
 import axios from "axios";
+import Popup from 'reactjs-popup';
+import DataFetchingComponent from './components/DataFetchingComponent';
+
 
 function App() {
     // const [currentFrom, setCurrentFrom] = useState('Home');
@@ -13,7 +16,7 @@ function App() {
     // }
     
     const [file, setFile] = useState(null);
-
+    
     const onFormSubmit = (e) =>{
         e.preventDefault();
 
@@ -43,11 +46,12 @@ function App() {
         setFile(e.target.files[0]);
     };
 
-    const onMouseOverCaptureHandler = () => {
-        alert('Image 1 Alert!!!')
+    // const onMouseOverHandler = () => {
+    //     alert('Image 1 Alert!!!')
         
-    };
-    
+    // };
+
+        
     return(
         // <div className="App">
         //     {
@@ -57,37 +61,246 @@ function App() {
         <div className="container">
 
             {/* grid */}
-            <div  className="grid1-1">
-                <img src="image/image1.jpg" alt="computer" onMouseOverCapture={onMouseOverCaptureHandler}/>
-            </div>
+            <Popup trigger=
+                {<div className="grid1-1">
+                    <img src="image/image1.jpg" alt="image1.jpg" />
+                </div>
+                }
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                            <DataFetchingComponent id={1} />
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        Close
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
 
-            <div  className="grid1-2">
-                <img src="image/image2.jpg" alt="computer"/>
-            </div>
+            <Popup trigger=
+                {<div className="grid1-2">
+                    <img src="image/image2.jpg" alt="image2.jpg" />
+                </div>
+                }
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                            <DataFetchingComponent id={2} />
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        Close
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
 
-            <div  className="grid1-3">
-                <img src="image/image3.jpg" alt="computer"/>
-            </div>
+            <Popup trigger=
+                {<div className="grid1-3">
+                    <img src="image/image3.jpg" alt="image3.jpg" />
+                </div>
+                }
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                            <DataFetchingComponent id={3} />
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        Close
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
 
-            <div  className="grid1-4">
-                <img src="image/image4.jpg" alt="computer"/>
-            </div>
+            <Popup trigger=
+                {<div className="grid1-4">
+                    <img src="image/image4.jpg" alt="image4.jpg" />
+                </div>
+                }
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                            <DataFetchingComponent id={4} />
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        Close
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
 
-            <div  className="grid1-5">
-                <img src="image/image5.jpg" alt="computer"/>
-            </div>
+            <Popup trigger=
+                {<div className="grid1-5">
+                    <img src="image/image5.jpg" alt="image5.jpg" />
+                </div>
+                }
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                            <DataFetchingComponent id={5} />
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        Close
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
 
-            <div  className="grid1-6">
-                <img src="image/image6.jpg" alt="computer"/>
-            </div>
+            <Popup trigger=
+                {<div className="grid1-6">
+                    <img src="image/image6.jpg" alt="image6.jpg" />
+                </div>
+                }
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                            <DataFetchingComponent id={6} />
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        Close
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
+
+            <Popup trigger=
+                {<div className="grid1-7">
+                    <img src="image/image7.jpg" alt="image7.jpg" />
+                </div>
+                }
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                            <DataFetchingComponent id={7} />
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        Close
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
+
+            <Popup trigger=
+                {<div className="grid1-8">
+                    <img src="image/image8.jpg" alt="image8.jpg" />
+                </div>
+                }
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                            <DataFetchingComponent id={8} />
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        Close
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
             
-        <br></br>
-        <form onSubmit={onFormSubmit}>
-            <input type="file"  onChange={onInputChange} />
-            <button type="submit">Upload</button>
-        </form>
+            <Popup trigger=
+                {<div className="grid1-9">
+                    <img src="image/image9.jpg" alt="image9.jpg" />
+                </div>
+                }
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                            <DataFetchingComponent id={9} />
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        Close
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
+
+            <Popup trigger=
+                {<div className="grid1-10">
+                    <img src="image/image10.jpg" alt="image10.jpg" />
+                </div>
+                }
+                modal nested>
+                {
+                    close => (
+                        <div className='modal'>
+                            <div className='content'>
+                            <DataFetchingComponent id={10} />
+                            </div>
+                            <div>
+                                <button onClick=
+                                    {() => close()}>
+                                        Close
+                                </button>
+                            </div>
+                        </div>
+                    )
+                }
+            </Popup>
+        
+            <div className="upload" >
+                <form onSubmit={onFormSubmit} >
+                    <input type="file"  onChange={onInputChange} />
+                    <br></br>
+                    <button type="submit">Upload</button>
+                </form>
+            </div>
 
         </div>
+        
     );
 }
 
---
+++ src/components/DataFetchingComponent.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const DataFetchingComponent = ({id}) => {
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(true);
+  
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(`http://192.168.10.76:8080/web/emp/${id}`);
+          setData(response.data);
+          setLoading(false);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          setLoading(false);
+        }
+      };
+  
+      fetchData();
+    }, [id]);
+  
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+  
+    return (
+      <div>
+        {data.map((item) => (
+            <div key={item.EmpID}>
+                <h1>Detail</h1>
+                <p>EmployeeID : {item.EmpID}</p>
+                <p>Name : {item.Name}</p>
+                <p>Position : {item.Position}</p>
+                <p>ComID : {item.ComID}</p>
+                <p>Email : {item.Email}</p>
+                <p>Status : {item.Status}</p>
+            </div> 
+        ))}
+      </div>
+    );
+  };
+  
+  export default DataFetchingComponent;
