da4b2298bb07808f737aa0d9638727f00a06b04a
teerapongchaemchamrat <126034013+teerapongchaemchamrat@users.noreply.github.com>
2024-08-05 11:58:23 +0700

    Update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,80 +1,12 @@
 import React from "react";
-//import React, { useState, useEffect } from "react";
 import "./Home.css";
-//import axios from "axios";
 
 function Home() {
 
   const getUsername = localStorage.getItem('Fullname');
-
-  // const [position, setPosition] = useState({ x: null, y: null });
-  // const [dragging, setDragging] = useState(false);
-  // const [firstClick, setFirstClick] = useState(true);
-
-  // useEffect(() => {
-  //   const handleMouseMove = (event) => {
-  //     if (dragging) {
-  //       setPosition({
-  //         x: event.clientX - event.target.width / 2,
-  //         y: event.clientY - event.target.height / 2
-  //       });
-  //     }
-  //   };
-
-  //   const handleMouseUp = () => {
-  //     //window.confirm("Do you want to save this position ?");
-  //     setDragging(false);
-  //     setFirstClick(true);
-  //     try{
-  //       //const response = axios.put("");
-
-  //     } catch (err) {
-  //       console.log("Error save this positon : " + err);
-  //     }
-      
-  //   };
-
-  //   window.addEventListener('mousemove', handleMouseMove);
-  //   window.addEventListener('mouseup', handleMouseUp);
-
-  //   return () => {
-  //     window.removeEventListener('mousemove', handleMouseMove);
-  //     window.removeEventListener('mouseup', handleMouseUp);
-  //   };
-  // }, [dragging]);
-
-  // const handleImageMouseDown = (event) => {
-  //   if (firstClick) {
-  //     const confirmDrag = window.confirm("Do you want to drag the image?");
-  //     if (!confirmDrag) {
-  //       return;
-  //     }
-  //     setFirstClick(false);
-  //   }
-
-  //   const initialX = event.clientX - event.target.width / 2;
-  //   const initialY = event.clientY - event.target.height / 2;
-
-  //   setPosition({ x: initialX, y: initialY });
-  //   setDragging(true);
-  // };
-  
   
   return (
     <div className="home">
-
-      {/* <img 
-        src="image/stat0.jpg"
-        alt="test"
-        onMouseDown={handleImageMouseDown}
-        style={{
-          cursor: dragging ? "grabbing" : "pointer",
-          position: "absolute",
-          left: `${position.x}px`,
-          top: `${position.y}px`,
-        }}
-        /> */}
-
         <div style={{position: "absolute", marginTop: "-20%"}}>
         <h1>WELCOME ! <br/> {getUsername}</h1>
         </div>
